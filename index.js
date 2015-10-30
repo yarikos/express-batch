3aa226da6765aee82896262fd241528edaaeef28
Aaron John Schlosser <aaron@aaronschlosser.com>
2015-10-29 20:39:01 -0700

    allow for nested field-value pairs in batched endpoints

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 "use strict";
 
+var url = require('url');
+
 var FakeRequest = require('./lib/FakeRequest');
 var FakeResponse = require('./lib/FakeResponse');
 
 module.exports = function expressBatch(app, options) {
 
+    options = options || {};
+
     return function (req, res) {
         var results = {};
-        var requests = req.query;
+        var requests = {};
+
+        // Attempt to parse the req.url object
+        var query = url.parse(req.url).query;
+
+        // If the separator option exists and the request URL can be parsed, parse any nested field-value pairs
+        if (options.separator && query) {
+            query = decodeURIComponent(query);
+            var quriesWithPairs = query.split(options.separator);
+            quriesWithPairs.forEach(function(query, i) {
+                requests[query.substring(0,query.indexOf('='))] = query.substring(query.indexOf('=')+1);
+            });
+        }
+
+        // Otherwise, use the req.query object as-is
+        else {
+            requests = req.query;
+        }
+
         var requestCount = Object.keys(requests).length;
         var finishedRequests = 0;
 
